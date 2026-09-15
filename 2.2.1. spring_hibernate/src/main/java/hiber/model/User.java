aab04159f70969505a774026040d443f9e892fa2
Anton Shevchuk <AntonShevchuk94@gmail.com>
2026-09-15 21:40:12 +0300

    Change method DI and change OneToOne position

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -7,11 +7,6 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-   @OneToOne(cascade = CascadeType.ALL)
-   @JoinColumn(name = "car_id")
-   private Car car;
-
-
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
@@ -25,6 +20,9 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id")
+   private Car car;
 
 
    public User() {}
